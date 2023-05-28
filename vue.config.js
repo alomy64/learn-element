76b71e48d569f9fc8cd5645f89ce0754c9c6e1c9
alomy64 <alomy64@163.com>
2023-05-28 14:38:20 +0800

    chore: 设置别名 @element

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  // chainWebpack: (config) => {
+  //   config.resolve.alias.set('@element', path.resolve(__dirname, 'src/components/element'));
+  // },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@element': '@/components/element',
+      },
+    },
+  },
 });
