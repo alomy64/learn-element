8853ca2b872de450a941d68c404062614692b9bb
alomy64 <alomy64@163.com>
2023-06-03 22:42:24 +0800

    chore(Eslint): 取消 multi-word-component-names 的校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vuejs-accessibility/no-autofocus': 'off',
+    'vue/multi-word-component-names': 'off',
   },
   overrides: [
     {
