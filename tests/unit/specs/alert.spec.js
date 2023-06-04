ccd5173869d3a8827154e9e0aaf8de46dfa6d226
alomy64 <alomy64@163.com>
2023-06-04 17:22:34 +0800

    deps: 替换测试插件为 Vitest

---
+++ tests/unit/specs/alert.spec.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Alert', () => {
+  it('create', () => {
+    function add(a, b) {
+      return a + b;
+    }
+
+    expect(add(1, 2)).toBe(3);
+  });
+});
